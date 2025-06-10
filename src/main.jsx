1dd0f69e167914e67c5a06da8e2f4178a6a3e78f
Priyanshi <shahpriyanshi1025@gmail.com>
2025-06-10 20:08:05 +0530

    different way of rendering react dom

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,16 @@ import './index.css'
 import App from './App.jsx'
 import MoneyState from './Context/MoneyState.jsx'
 
+// different way of rendering ReactDOM
 createRoot(document.getElementById('root')).render(
   <MoneyState>
     <App />
   </MoneyState>,
 )
+
+// const elem = createRoot(document.getElementById('root'));
+// elem.render(
+//   <MoneyState>
+//     <App />
+//   </MoneyState>,
+// );
